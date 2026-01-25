731bbef1595b2865f8517c31b44fb668b0b5a845
Joie <cassieye20190909@gmail.com>
2026-01-25 14:55:10 +0800

    map-container中引入高德地图

---
+++ src/pages/map-container.vue
@@ -0,0 +1,39 @@
+<script setup>
+import AMapLoader from '@amap/amap-jsapi-loader'
+
+const map = ref(null)
+
+onMounted(() => {
+  window._AMapSecurityConfig = {
+    securityJsCode: 'bf61b5b8457d13555c3d9e36321102b1',
+  }
+  AMapLoader.load({
+    key: 'bf61b5b8457d13555c3d9e36321102b1',
+    version: '2.0',
+    plugins: ['AMap.Scale'],
+  })
+    .then((Amap) => {
+      map.value = new Amap.Map('container', {
+        viewMode: '3D',
+        zoom: 11,
+        center: [116.397428, 39.90923],
+      })
+    })
+    .catch((e) => {
+      console.error(e)
+    })
+})
+</script>
+
+<template>
+  <div id="container" />
+</template>
+
+<style scoped>
+#container {
+  padding: 0px;
+  margin: 0px;
+  width: 100%;
+  height: 800px;
+}
+</style>
--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -22,6 +22,7 @@ declare module 'vue-router/auto-routes' {
     '/[...all]': RouteRecordInfo<'/[...all]', '/:all(.*)', { all: ParamValue<true> }, { all: ParamValue<false> }>,
     '/about': RouteRecordInfo<'/about', '/about', Record<never, never>, Record<never, never>>,
     '/hi/[name]': RouteRecordInfo<'/hi/[name]', '/hi/:name', { name: ParamValue<true> }, { name: ParamValue<false> }>,
+    '/map-container': RouteRecordInfo<'/map-container', '/map-container', Record<never, never>, Record<never, never>>,
     '/README': RouteRecordInfo<'/README', '/README', Record<never, never>, Record<never, never>>,
   }
 }
